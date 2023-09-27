90614d09629e1291273d56ba48d0e45b54f8c694
NurAyuL27 <nurayu2718@gmail.com>
2023-09-27 09:45:29 +0700

    coba

--- pt 5/PemilihanPercobaan121.java
+++ pt 5/PemilihanPercobaan121.java
@@ -1,13 +1,12 @@
 import java.util.Scanner;
 public class PemilihanPercobaan121 {
     public static void main(String[] args) {
-            Scanner input21 = new 
-Scanner(System.in);
-            String hasil;
+            Scanner input21 = new Scanner(System.in);
         System.out.print("Masukkan angka:  ");
         int angka = input21.nextInt();
-        hasil = (angka % 2 == 0)?
-"Bilangan Genap" : "Bilangan negatif";
-        System.out.println(angka + "adalah " + hasil);
+        if (angka % 2 == 0)
+        System.out.println("Angka"+angka+"bilangan genap");
+        else
+        System.out.println("Angka "+angka+" bilangan ganjil");
      }
 }
