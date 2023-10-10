a3aeb0524bcfb5448ae4540603572367b4bdc8d6
NurAyuL27 <nurayu2718@gmail.com>
2023-10-10 10:39:35 +0700

    coba

--- pt6/Pemilihan2Percobaan321.java
+++ pt6/Pemilihan2Percobaan321.java
@@ -11,8 +11,8 @@ public class Pemilihan2Percobaan321 {
         System.out.println("Masukkan Besarnya penghasilan :");
         penghasilan = input21.nextInt();
         
-        if (kategori.equalsIgnoreCase("pekerja")) {
-            if (penghasilan <= 2000000) 
+        if (kategori.equals("pekerja")) {
+            if (penghasilan <= 2500000) 
                 pajak = 0.1; 
             else if (penghasilan <= 3000000) 
                 pajak = 0.15;
@@ -20,8 +20,8 @@ public class Pemilihan2Percobaan321 {
                 pajak = 0.2;
             gajiBersih = (int) (penghasilan - (pajak * penghasilan)); 
             System.out.print("Penghasil Bersih : " + gajiBersih);
-        } else if (kategori.equalsIgnoreCase("pebisnis")) {
-            if (penghasilan <= 2500008) 
+        } else if (kategori.equals("pebisnis")) {
+            if (penghasilan <= 2000000) 
                 pajak = 0.15; 
             else if (penghasilan <= 3500000) 
                 pajak = 0.2; 
