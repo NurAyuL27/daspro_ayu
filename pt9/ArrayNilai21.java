e2ec725bda280a274f530e43920f0fd449397ed1
NurAyuL27 <nurayu2718@gmail.com>
2023-10-24 20:19:29 +0700

    tugas 2.2 pertanyaan 5

--- pt9/ArrayNilai21.java
+++ pt9/ArrayNilai21.java
@@ -6,13 +6,15 @@ public class ArrayNilai21 {
         Scanner input21 = new Scanner(System.in);
         int[] nilaiAkhir = new int[10];
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir.length; i++) { 
             System.out.print("Masukkan nilai akhir ke-" + i + " ");
             nilaiAkhir[i] = input21.nextInt();
+        
+            if (nilaiAkhir[i] > 70) { 
+                System.out.println("Mahasiswa ke-" + i + " lulus!"); 
+            } else {
+                System.out.println("Mahasiswa ke-" + i + " tidak lulus!");
+            }
         }
-
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiAkhir[i]);
-        }
-    }
+    }    
 }
