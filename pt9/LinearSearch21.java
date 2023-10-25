7a087ad164233f27066fb3381996a9d8ffc30797
NurAyuL27 <nurayu2718@gmail.com>
2023-10-25 11:47:09 +0700

    coba

--- pt9/LinearSearch21.java
+++ pt9/LinearSearch21.java
@@ -1,22 +1,36 @@
 package pt9;
-
 import java.util.Scanner;
-public class LinearSearch21 {
-        
-        public static void main(String[] args) {
 
-        int[] arrayInt = {34, 18, 26, 48, 12, 20, 56, 63};
-        int key = 20;
-        int hasil = 0;
+public class LinearSearch21 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int jmlArray, key;
+        int hasil = -1; 
+        System.out.print("Masukkan jumlah elemen Array : ");
+        jmlArray = sc.nextInt();
+        int[] arrayInt = new int[jmlArray];
 
         for (int i = 0; i < arrayInt.length; i++) {
-            if (arrayInt[i] == key) {
+            System.out.print("Masukkan elemen array ke-" + i + " : ");
+            arrayInt[i] = sc.nextInt();
+        }
+
+        System.out.print("Masukkan key yang ingin dicari: ");
+        key = sc.nextInt();
+
+        for (int i = 0; i < arrayInt.length; i++) {
+            if (arrayInt[i] == key) { 
                 hasil = i;
                 break;
             }
         }
-        System.out.println("Key ada dalam array pada posisi indeks ke-"+hasil);
 
+        if (hasil != -1) {
+            System.out.println("Key ditemukan pada indeks: " + hasil);
+        } else {
+            System.out.println("Key tidak ditemukan dalam array.");
+        }
     }
-
 }
