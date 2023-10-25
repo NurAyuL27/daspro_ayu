004e0f907959a99cfeea65b84e71fe518842daa0
NurAyuL27 <nurayu2718@gmail.com>
2023-10-25 09:39:37 +0700

    tugas 2.4 pertanyaan 3

---
+++ pt9/BubbleDescendingExample21.java
@@ -0,0 +1,25 @@
+package pt9;
+import java.util.Scanner;
+
+public class BubbleDescendingExample21 {
+    public static void main(String[] args) {
+        int[] intData = {34, 18, 87, 72, 32, 54, 43};
+        int temp = 0;
+
+        for (int i = 0; i < intData.length; i++) {
+            for (int j = 1; j < intData.length - i; j++) {
+                if (intData[j - 1] < intData[j]) {
+                    temp = intData[j];
+                    intData[j] = intData[j - 1];
+                    intData[j - 1] = temp;
+                }
+            }
+        }
+
+        System.out.println("Hasil pengurutan descending: ");
+        for (int i = 0; i < intData.length; i++) {
+            System.out.println(intData[i]);
+        }
+    }
+
+}
