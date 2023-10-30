abf34e101f26ffd89c084e06da0d2b8f2f2f4b8b
NurAyuL27 <nurayu2718@gmail.com>
2023-10-31 05:19:52 +0700

    coba

---
+++ pt10/Numbers21.java
@@ -0,0 +1,17 @@
+package pt10;
+import java.util.Arrays;
+import java.util.Scanner;
+public class Numbers21 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] myNumbers = new int[3][5]; 
+
+        for (int i = 0; i < myNumbers.length; i++) { 
+            System.out.println(Arrays.toString(myNumbers[i]));
+        }
+        
+        myNumbers[0] = new int[5];
+        myNumbers [1]= new int[3];
+        myNumbers [2] = new int[1];
+    }
+}
