6f06251376219e67e6723a1f86c288256bd25f65
NurAyuL27 <nurayu2718@gmail.com>
2023-10-31 06:56:38 +0700

    percobaan 1 no

--- pt10/Numbers21.java
+++ pt10/Numbers21.java
@@ -4,14 +4,16 @@ import java.util.Scanner;
 public class Numbers21 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[][] myNumbers = new int[3][5]; 
+        int[][] myNumbers = new int[3][5];
+        
+        myNumbers[0] = new int[5];
+        myNumbers [1]= new int[3];
+        myNumbers [2] = new int[1];
 
         for (int i = 0; i < myNumbers.length; i++) { 
             System.out.println(Arrays.toString(myNumbers[i]));
         }
         
-        myNumbers[0] = new int[5];
-        myNumbers [1]= new int[3];
-        myNumbers [2] = new int[1];
+        
     }
 }
