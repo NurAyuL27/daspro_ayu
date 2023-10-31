1cfb7612b01f1c5d490be60f1149e09e2b23c2f5
NurAyuL27 <nurayu2718@gmail.com>
2023-10-31 20:27:30 +0700

    Percobaan 1 Pertanyaan 6

--- pt10/Bioskop21.java
+++ pt10/Bioskop21.java
@@ -15,8 +15,8 @@ public class Bioskop21 {
 
         System.out.println(penonton.length);
 
-        for (int i = 0; i < penonton.length; i++) {
-            System.out.println("Panjang baris ke-" + (i + 1) + ": " + penonton[i].length);
+        for (String[] barisPenonton : penonton) {
+            System.out.println("Panjang baris ke: " + barisPenonton.length);
         }
 
             System.out.printf("%s \t %s\n", penonton [0][0], penonton[0][1]); 
